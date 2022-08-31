601035e7e464e018ba0635f8dd821120392431fc
VongaiTecvator01 <vongai@tecvator.com>
2022-08-31 19:19:53 +0200

    improved responsiveness

--- src/components/pages/login.jsx
+++ src/components/pages/login.jsx
@@ -1,25 +1,29 @@
 import React from "react";
 import { useNavigate } from "react-router-dom"
+import Header from "../partials/header";
+import Footer from "../partials/footer";
 
 const Login = () => {
     const navigate = useNavigate();
     return (
-
+        
         <div className="form-body">
+            <Header/>
             <form className="form-container">
-                <div className="input-container">
-                    <label>Username </label>
-                    <input type="text" name="uname" required />
-                </div>
-                <div className="input-container">
-                    <label>Password </label>
-                    <input type="password" name="pass" required />
-                </div>
                 <div>
+                    <label className="form__label" >Username</label>
+                    <input className="form__input" type="text" name="uname" placeholder="Email or Username" required />
+                </div>
+                <div >
+                    <label className="form__label" >Password</label>
+                    <input className="form__input" type="password" name="pass" placeholder="Password" required />
+                </div>
+                <div className="form-btns">
                     <button type="submit" class="btn" onClick={() => navigate('/')}>Back</button>
                     <button type="submit" class="btn">Login</button>
                 </div>
             </form>
+            <Footer/>
         </div>
     )
 }
--- src/components/pages/register.jsx
+++ src/components/pages/register.jsx
@@ -1,35 +1,30 @@
 import React from "react";
 import Footer from "../partials/footer";
+import Header from "../partials/header";
 import { useNavigate } from "react-router-dom"
 
 const Register = () => {
     const navigate = useNavigate();
     return (
-        <div className="bg">
-            <div className="form-container form-body">
-                <div className="username">
-                    <label className="form__label" for="firstName">First Name </label>
-                    <input className="form__input" type="text" id="firstName" placeholder="First Name" />
-                </div>
-                <div className="lastname">
-                    <label className="form__label" for="lastName">Last Name </label>
-                    <input type="text" name="" id="lastName" className="form__input" placeholder="LastName" />
-                </div>
-                <div className="email">
+        <div className="form-body">
+            <Header/>
+            <div className="form-container ">
+                <div >
                     <label className="form__label" for="email">Email </label>
                     <input type="email" id="email" className="form__input" placeholder="Email" />
                 </div>
-                <div className="password">
+                <div >
                     <label className="form__label" for="password">Password </label>
                     <input className="form__input" type="password" id="password" placeholder="Password" />
                 </div>
-                <div className="confirm-password">
+                <div >
                     <label className="form__label" for="confirmPassword">Confirm Password </label>
                     <input className="form__input" type="password" id="confirmPassword" placeholder="Confirm Password" />
                 </div>
-                <div>
+                <div className="form-btns">
                     <button type="submit" class="btn" onClick={() => navigate('/')}>Back</button>
-                    <button type="submit" class="btn">Register</button></div>
+                    <button type="submit" class="btn">Register</button>
+                </div>
             </div>
             <Footer />
         </div>
